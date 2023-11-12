a8bb7aba16832d69ea723f52dd1146c6c48cfaa4
Bagas Dewanusa <aryasenabagas.work@gmail.com>
2023-11-12 20:27:11 +0700

    inserted some bootstrap elements. added some tools for test but stilldon't get it: postponed.

--- luckybun/luckybunapp/models.py
+++ luckybun/luckybunapp/models.py
@@ -1,18 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+
 class Item(models.Model):
-    class Type(models.TextChoices):
-        INGREDIENT = 'Ingredient', 'Ingredient'
-        TOOL = 'Tool', 'Tool'
-        INVENTORY = 'Inventory', 'Inventory'
+    options = {
+        ("ingredient", "Ingredient"),
+        ("tool", "Tool"),
+        ("inventory", "Inventory"),
+    }
 
     ItemID = models.IntegerField(primary_key=True)
     Name = models.CharField(max_length=255)
     Description = models.TextField()
-    Type = models.CharField(max_length=20, choices=Type.choices)
+    Type = models.CharField(max_length=20, choices=options, default="ingredient")
     Quantity = models.IntegerField()
+
 
 class Product(models.Model):
     ProductID = models.IntegerField(primary_key=True)
@@ -21,6 +25,7 @@ class Product(models.Model):
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     StockQuantity = models.IntegerField()
 
+
 class Production(models.Model):
     ProductionID = models.IntegerField(primary_key=True)
     ProductID = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -28,10 +33,12 @@ class Production(models.Model):
     ProductionDate = models.DateField()
     ExpirationDate = models.DateField()
 
+
 class ProductItems(models.Model):
     ProductID = models.ForeignKey(Product, on_delete=models.CASCADE)
     ItemID = models.ForeignKey(Item, on_delete=models.CASCADE)
     RequiredQuantity = models.IntegerField(primary_key=True)
+
 
 class Customer(models.Model):
     CustomerID = models.IntegerField(primary_key=True)
@@ -40,38 +47,44 @@ class Customer(models.Model):
     Location = models.CharField(max_length=255)
     Notes = models.TextField()
 
-class Orders(models.Model):
-    class Status(models.TextChoices):
-        PENDING = 'Pending', 'Pending'
-        PROCESSING = 'Processing', 'Processing'
-        SHIPPED = 'Shipped', 'Shipped'
-        COMPLETED = 'Completed', 'Completed'
 
-    Status = models.CharField(max_length=20, choices=Status.choices)
+class Orders(models.Model):
+    options = {
+        ("pending", "Pending"),
+        ("processing", "Processing"),
+        ("shipped", "Shipped"),
+        ("completed", "Completed"),
+    }
+
+    Status = models.CharField(max_length=20, choices=options, default="pending")
     OrderID = models.IntegerField(primary_key=True)
     OrderDate = models.DateField()
     CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
 
 class OrderItems(models.Model):
     OrderID = models.ForeignKey(Orders, on_delete=models.CASCADE)
     ProductID = models.ForeignKey(Product, on_delete=models.CASCADE)
     Quantity = models.IntegerField(primary_key=True)
 
+
 class Shipment(models.Model):
-    class ShipmentStatus(models.TextChoices):
-        PARTIAL = 'Partial', 'Partial'
-        COMPLETE = 'Complete', 'Complete'
+    options = {
+        ("partial", "Partial"),
+        ("complete", "Complete"),
+    }
 
-
-    ShipmentStatus = models.CharField(max_length=20, choices=ShipmentStatus.choices)
+    ShipmentStatus = models.CharField(max_length=20, choices=options, default="partial")
     ShipmentID = models.IntegerField(primary_key=True)
     ShipmentDate = models.DateField()
     OrderID = models.ForeignKey(Orders, on_delete=models.CASCADE)
+
 
 class ShipmentItems(models.Model):
     ShipmentID = models.ForeignKey(Shipment, on_delete=models.CASCADE)
     ProductID = models.ForeignKey(Product, on_delete=models.CASCADE)
     QuantityShipped = models.IntegerField(primary_key=True)
+
 
 class ProductReturn(models.Model):
     ReturnID = models.IntegerField(primary_key=True)
@@ -83,9 +96,11 @@ class ProductReturn(models.Model):
     QuantityReturned = models.IntegerField()
     ProductionID = models.ForeignKey(Production, on_delete=models.CASCADE)
 
+
 class Account(models.Model):
     AccountID = models.IntegerField(primary_key=True)
     AccountName = models.CharField(max_length=255)
+
 
 class SalesTransaction(models.Model):
     SaleID = models.IntegerField(primary_key=True)
@@ -93,6 +108,7 @@ class SalesTransaction(models.Model):
     TransactionDate = models.DateField()
     Amount = models.DecimalField(max_digits=10, decimal_places=2)
     AccountID = models.ForeignKey(Account, on_delete=models.CASCADE)
+
 
 class RefundTransaction(models.Model):
     RefundTransactionID = models.IntegerField(primary_key=True)
@@ -102,6 +118,7 @@ class RefundTransaction(models.Model):
     RefundAmount = models.DecimalField(max_digits=10, decimal_places=2)
     AccountID = models.ForeignKey(Account, on_delete=models.CASCADE)
 
+
 class ExpenseTransaction(models.Model):
     ExpenseID = models.IntegerField(primary_key=True)
     TransactionDate = models.DateField()
@@ -109,10 +126,12 @@ class ExpenseTransaction(models.Model):
     Description = models.TextField()
     AccountID = models.ForeignKey(Account, on_delete=models.CASCADE)
 
+
 class Employee(models.Model):
     EmployeeID = models.IntegerField(primary_key=True)
     Name = models.CharField(max_length=255)
     Salary = models.DecimalField(max_digits=10, decimal_places=2)
+
 
 class SalaryTransaction(models.Model):
     SalaryID = models.IntegerField(primary_key=True)
@@ -120,6 +139,7 @@ class SalaryTransaction(models.Model):
     TransactionDate = models.DateField()
     Amount = models.DecimalField(max_digits=10, decimal_places=2)
     AccountID = models.ForeignKey(Account, on_delete=models.CASCADE)
+
 
 class ItemPurchase(models.Model):
     PurchaseID = models.IntegerField(primary_key=True)
@@ -129,3 +149,27 @@ class ItemPurchase(models.Model):
     ExpirationDate = models.DateField()
     AmountPaid = models.DecimalField(max_digits=10, decimal_places=2)
     ExpenseID = models.ForeignKey(ExpenseTransaction, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    options = {
+        ("draft", "Draft"),
+        ("published", "Published"),
+    }
+
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="post_author"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=options, default="draft")
+
+    class Meta:
+        ordering = ("created_at",)
+
+    def __str__(self):
+        return self.title
--- luckybun/luckybunapp/views.py
+++ luckybun/luckybunapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+from .models import Post
+
 
 # Create your views here.
 def home(request):
     return HttpResponse("hello world!")
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = "index.html"
